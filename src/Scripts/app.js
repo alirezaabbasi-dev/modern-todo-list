6a7a9220c8ffa88fb256ee608014fc5c67a76fd2
alirezaabbasi-dev <alirezaabbasi001@outlook.com>
2025-04-13 17:31:01 +0330

    added icon

--- src/Scripts/app.js
+++ src/Scripts/app.js
@@ -1,5 +1,6 @@
 "use strict";
 const $ = document;
+
 // todos header
 const textInput = $.querySelector("#textInput");
 const btnAdd = $.querySelector("#btnAdd");
@@ -14,7 +15,7 @@ function addTodo() {
   if (!todoText) return;
 
   const todoObj = {
-    id: items.length + 1,
+    id: items.length ? items[items.length - 1].id + 1 : 1,
     title: todoText,
     isDone: false,
   };
@@ -27,53 +28,70 @@ function addTodo() {
 }
 
 function renderTodoItem(item) {
-  todosContainer.innerHTML += `
-    <div
-      class="todo-item w-full max-h-[75px] rounded-xl text-zinc-700 bg-zinc-100/20 shadow-md shadow-black/30 p-2.5 flex items-center justify-between wow animate__animated animate__fadeInUp">
-      <div data-id="${
-        item.id
-      }" class="todo-item-content text-wrap wrap-anywhere line-clamp-3 todo-text ${
+  const todoElement = document.createElement("div");
+  todoElement.className =
+    "todo-item w-full max-h-[75px] rounded-xl text-zinc-700 bg-zinc-100/20 shadow-md shadow-black/30 p-2.5 flex items-center justify-between wow animate__animated animate__fadeInUp";
+
+  todoElement.innerHTML = `
+    <div data-id="${
+      item.id
+    }" class="todo-item-content text-wrap wrap-anywhere line-clamp-3 todo-text ${
     item.isDone ? "line-through" : ""
   }">
-           ${item.title}
-      </div>
-      <div class="btn-container ml-1 flex items-center justify-end flex-wrap gap-2 ">
-          <button onClick="doneBtnHandler(event)" class="done-btn w-20 h-7 rounded-3xl text-emerald-600 ${
-            item.isDone ? "" : "bg-emerald-300"
-          }">${item.isDone ? "Undo" : "Done"}</button>
-          <button onClick="deleteBtnHandler(event)" class="delete-btn w-20 h-7 rounded-3xl text-red-600 bg-red-300">Delete</button>
-      </div>
+      ${item.title}
+    </div>
+    <div class="btn-container ml-1 flex items-center justify-end flex-wrap gap-2">
+      <button title="done todo" class="done-btn max-w-20 h-7 flexAllCenter text-emerald-300">
+        <svg class='w-5 h-5 '><use href='${
+          item.isDone ? "#undo" : "#check"
+        }'></use></svg>
+      </button>
+      <button title="delete todo" class="delete-btn flexAllCenter w-20 h-7 text-red-500">
+        <svg class='w-5 h-5 '><use href='#delete-icon'></use></svg>
+      </button>
     </div>
   `;
+
+  // event handler
+  const doneBtn = todoElement.querySelector(".done-btn");
+  const deleteBtn = todoElement.querySelector(".delete-btn");
+
+  doneBtn.addEventListener("click", doneBtnHandler);
+  deleteBtn.addEventListener("click", deleteBtnHandler);
+
+  todosContainer.appendChild(todoElement);
 }
 
 function deleteBtnHandler(event) {
-  const id = +event.target.parentElement.previousElementSibling.dataset.id;
+  const todoElement = event.target.closest(".todo-item");
+  const id = +todoElement.querySelector("[data-id]").dataset.id;
+
   items = items.filter((item) => item.id !== id);
   localStorage.setItem("todos", JSON.stringify(items));
-  event.target.parentElement.parentElement.remove();
+  todoElement.remove();
 }
 
 function doneBtnHandler(event) {
-  const todoElem = event.target.parentElement.previousElementSibling;
-  const id = +todoElem.dataset.id;
+  const todoElement = event.target.closest(".todo-item");
+  const todoText = todoElement.querySelector("[data-id]");
+  const id = +todoText.dataset.id;
+
   const item = items.find((item) => item.id === id);
   item.isDone = !item.isDone;
-
   localStorage.setItem("todos", JSON.stringify(items));
 
-  todoElem.classList.toggle("line-through");
-  event.target.innerText = item.isDone ? "Undo" : "Done";
-  event.target.classList.toggle("bg-emerald-300");
+  // تغییر کلاس خط‌خورده
+  todoText.classList.toggle("line-through");
+
+  // change icons
+  const icon = event.target.closest("button").querySelector("use");
+  icon.setAttribute("href", item.isDone ? "#undo" : "#check");
 }
 
 function getFromLocalStorage() {
   const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
   items = savedTodos;
-
-  items.forEach((item) => {
-    renderTodoItem(item);
-  });
+  items.forEach(renderTodoItem);
 }
 
 window.addEventListener("load", getFromLocalStorage);
